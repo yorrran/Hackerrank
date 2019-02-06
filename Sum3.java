625bdd38b371c6935f98568e59176bdf23d94c8e
yorrran <pangyaran@hotmail.sg>
2019-02-06 19:33:57 +0800

    Update Sum3.java

--- Sum3.java
+++ Sum3.java
@@ -1,34 +1,28 @@
 import java.io.*;
 import java.util.*;
 
-public static void solution() {
-		int[] arr = new int[] {0, -1, 2, -3, 1};
-		List<List<Integer>> res = new ArrayList<List<Integer>>(); 
-		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-		
-		for(int i = 0; i < arr.length; i++) {
-			map.put(arr[i], arr[i]);
-		}
-		for (int i = 0; i<arr.length; i++) {
-			for(int j = i+1; j < arr.length; j++) {
-				int b = 0-(arr[i]+arr[j]);
-				if(map.containsKey(b)) {
-					List<Integer> cur = new ArrayList<Integer>();
-					cur.add(map.get(b));
-					cur.add(arr[i]);
-					cur.add(arr[j]);
-					res.add(cur);
+public static List<List<Integer>> solution() {
+	int[] arr = new int[] {-1,0,1, 2, 4};
+	HashSet<List<Integer>> res = new HashSet<List<Integer>>();
+	Arrays.sort(arr);
+	if (arr.length < 3) {
+		return new ArrayList<>(res);
+	}
+	for (int i = 0; i < arr.length - 2; i++) {
+		int j = i + 1;
+		int k = arr.length - 1;
+		while (j < k) {
+			int sum = arr[i] + arr[j] + arr[k];
+			if (sum == 0)
+				res.add(Arrays.asList(arrr[i], arr[j++], arr[k--]));
+			else if (sum < 0) {
+				j++;
+			} else {
+				k--;
 			}
-				
+
 		}
 	}
-		HashSet<List<Integer>> set = new HashSet<List<Integer>>();
-		for(List li: res) {
-			Collections.sort(li);
-			set.add(li);
-		}
-		res.clear();
-		res.addAll(set);
-		System.out.println("the triplet:"+ res);
+	return new ArrayList<>(res);
 	
-	 }
+ }
